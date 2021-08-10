253875087cadacb512794958749a1b09585d5d0f
natkozak <natkozak0@gmail.com>
2021-08-09 21:09:05 -0700

    Fresh entrypoint.sh

--- action-reaction/entrypoint.sh
+++ action-reaction/entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/sh -l
 
 # shell script that counts reactions goes here
-
-curl \
-  -H "Accept: application/vnd.github.squirrel-girl-preview+json" \
-  https://api.github.com/orgs/ORG/teams/TEAM_SLUG/discussions/42/comments/42/reactions
