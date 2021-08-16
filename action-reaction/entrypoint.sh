38cf93b06d40cd8637a8272936db2cde07cbf40c
natkozak <natkozak0@gmail.com>
2021-08-16 14:16:24 -0700

    Get reactions on issue 1

--- action-reaction/entrypoint.sh
+++ action-reaction/entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -l
 
 # shell script that counts reactions goes here
+
+curl \
+  -X GET \
+  -H "Accept: application/vnd.github.squirrel-girl-preview" \
+  https://api.github.com/repos/natkozak/NomicBot/issues/1/reactions
